9fe2b43cab697ebe6e6b1ddaa6d5dc1cdac40574
mark <bingpeng.liao@yingheying.com>
2018-09-13 17:25:12 +0800

    优化es6.html

--- js/es6.js
+++ js/es6.js
@@ -1,27 +1,39 @@
 (function(context){
   return context.extend(context,{
     config: {
-      aside: '#aside_content',
-      content: '#main_content',
-      backTop: '#back_top',
+      elements: {
+        aside: $('#aside_content'),
+        content: $('#main_content'),
+        backTop: $('#back_top'),
+        loadText: $("#load_text"),
+        progress: $("#progress")
+      },
       cache: {},
       cacheKey: 'MARKLBP_ES6_HANDBOOK',
-      list: []
+      list: [],
+      events: {
+        click: 'ontouchstart' in document ? 'touchend' : 'click',
+        mouseover: 'ontouchstart' in document ? 'touchstart' : 'mouseover',
+        mouseout: 'ontouchstart' in document ? 'touchend' : 'mouseout',
+      }
     },
     init: function (){
-      this.updateCache().initEvents().renderContent(this.config.aside, 'es6').updateContent()
+      this.updateCache().initEvents().renderContent(this.config.elements.aside, 'es6').updateContent()
       var file = decodeURI(location.hash.split('#')[2])
-      $(this.config.content).find('li.link[data-id=' + file + ']').trigger('click')
+      this.config.elements.content.find('li.link[data-id=' + file + ']').trigger(this.config.events.click)
       return this
     },
     initEvents: function (){
-      var click = 'ontouchstart' in document ? 'touchend' : 'click'
       var that = this
-      var $backTop = $(that.config.backTop)
-      var showBackTop = false
+      var click = this.config.events.click
+      var mouseover = this.config.events.mouseover
+      var mouseout = this.config.events.mouseout
       $(window).on('hashchange', $.proxy(this.updateContent, this)).on('scroll', function(){
         var sT = $(this).scrollTop()
-        $backTop[sT > 0 ? 'show' : 'hide']()
+        that.config.elements.backTop.css("display", sT > 0 ? 'inline-block' : 'none')
+        var sH = document.documentElement.scrollHeight
+        var cH = $(this).height()
+        that.config.elements.progress.width(that.toFixed(sT/(sH - cH)*10000, 2) + '%')
       })
       $(document).on(click, 'a.section-link', function(e){
         e.preventDefault();
@@ -36,13 +48,16 @@
         that.goTop()
       }).on(click, "#btn_menu", function(e){
         e.preventDefault()
-        $(that.config.aside).parent().toggleClass("show")
+        that.config.elements.aside.parent().toggleClass("show")
       }).on(click, "#page_prev, #page_next", function(){
+        this.clickTimeStamp = this.clickTimeStamp || 0
+        if($.now() - this.clickTimeStamp < 1000)return that.modal.tip("翻页太快哦~", 500, true)
+        this.clickTimeStamp = $.now()
         var list = that.config.list
         var current = location.hash.split("#")[1] || list[0]
         var to
         if (list.length <= 0) {
-          list = [].slice.call($(that.config.aside).find("a").map(function(){return this.href.split("#")[1]}))
+          list = [].slice.call(that.config.elements.aside.find("a").map(function(){return this.href.split("#")[1]}))
         }
         if (this.id === 'page_prev') {
           to = list[list.indexOf(current) - 1] || list[0]
@@ -63,12 +78,13 @@
             $(this).animate({color: original_color}, 2500);
           });
           history.pushState(null, null, '#' + (location.hash.split('#')[1] || 'intro') + '#' + this.dataset.id);
-      }).on("mouseover mouseout", this.config.content + " h2," + this.config.content + " h3," + this.config.content + " h4", function (e) {
+      }).on(mouseover + " " + mouseout, "#main_content h2,#main_content h3,#main_content h4", function (e) {
+        e.preventDefault()
         if (this.resetTime) clearTimeout(this.resetTime)
-        if (e.type === 'mouseover') {
+        if (e.type === mouseover) {
           $(this).find("a").show()
         } else {
-          this.resetTime = setTimeout($.proxy(function(){$(this).find("a").hide()}, this), 500);
+          this.resetTime = setTimeout($.proxy(function(){$(this).find("a").hide()}, this), e.type.indexOf("touch") > -1 ? 3000 : 300);
         }
       })
       return this
@@ -78,16 +94,22 @@
       if(!file || file.length <= 0){
         file = 'intro'
       }
-      return this.renderContent(this.config.content, file)
+      return this.renderContent(this.config.elements.content, file)
     },
-    renderContent: function(selector, file){
+    renderContent: function(jqInstance, file){
+      this.toggleLoad(1)
+      this.config.elements.backTop.trigger(this.config.events.click)
       if (this.config.cache[file]) {
-        $(selector).html(this.config.cache[file])
+        jqInstance.html(this.config.cache[file])
+        this.config.elements.loadText.hide()
         return this
       }
       return this.ajax({
         url: (file === 'es6' ? '/javascript/es6/' : '/javascript/es6/docs/') + file + '.md',
         type: 'get',
+        always: function () {
+          this.toggleLoad(0)
+        },
         done: function(data){
           var str = marked(data)
           var dom = $('<div class="marked-text">' + str + '</div>');
@@ -100,12 +122,15 @@
             this.config.list = [].slice.call(dom.find("a").map(function(){return this.href.split("#")[1]}))
           }
           this.updateCache({key: file, value: dom[0].outerHTML})
-          $(selector).html(dom)
+          jqInstance.html(dom)
         },
         fail: function(data){
-          $(selector).html(data.value)
+          jqInstance.html(data.value)
         }
       })
+    },
+    toggleLoad: function (flag) {
+      this.config.elements.loadText[flag ? 'show' : 'hide']()
     },
     createAnchors: function(dom){
       var hash = location.hash.split('#')[1] || 'intro'
@@ -115,7 +140,7 @@
           var content = $(this).text()
           this.id = content.replace(/, /g, ',').replace(/[&\/\\#,.+=$~%'":*?<>{}\ \]\[]/g, "-").replace(/[()]/g, '')
           this.innerHTML = content +
-            ' <a style="display: none" href="#' + hash + '#' + this.id + '" class="section-link" data-content="' + this.id + '" data-hash="' + hash + '">§</a> <a href="javascript:void(0)" style="display: none" class="go-top">⇧</a>'
+            ' <a style="display: none" href="#' + hash + '#' + this.id + '" class="section-link" data-content="' + this.id + '" data-hash="' + hash + '">🔗</a> <a href="javascript:void(0)" style="display: none" class="go-top">👆</a>'
           this.setAttribute('data-content', content)
           if(lis){
             lis.push('<li data-id="' + this.id + '" class="link"><a href="#' + hash + '#' + content + '">' + content + '</a></li>')
